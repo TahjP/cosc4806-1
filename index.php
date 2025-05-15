b100ea5b6ecb36eccd88a7dc5aa755f5291ee9b3
tahj-anthony plummer <42690026-tplummer1@users.noreply.replit.com>
2025-05-15 21:48:24 +0000

    Added failed attempts session and redirects page

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,6 +11,8 @@
   
   <body>
     <h1>Assignment 1</h1>
+
+    <p>Welcome, <?=$_SESSION['username'] ?></p>
 
     <p><a href="/login.php">Click here to login </a><p>
  
